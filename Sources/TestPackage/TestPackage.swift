6feffde8b6fc755ac001fdf176df169e50251e2f
Mathew Sanders <mathew.sanders@gmail.com>
2019-09-27 13:26:11 -0400

    Add public initalizer.

--- Sources/TestPackage/TestPackage.swift
+++ Sources/TestPackage/TestPackage.swift
@@ -1,6 +1,10 @@
 public struct TestPackage {
     public var text = "Hello, World!"
     
+    public init() {
+        print("init test package")
+    }
+    
     public func double(number num: Int) -> Int {
         return num * 2
     }
